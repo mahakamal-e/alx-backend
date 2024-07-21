387afd7fd6da89a53ce6ceee6e239299c0ce0ac3
mahakamal-e <mahakamal09@gmail.com>
2024-07-20 18:12:04 -0700

    task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" Define index_range function. """
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Calculate the start and end index for pagination.
+
+    Args:
+    page (int): The current page number (1-indexed).
+    page_size (int): The number of items per page.
+
+    Returns:
+    tuple: A tuple containing the start index and end index for the items,
+    on the current page
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+    """
+    Retrieve a page of data from the dataset.
+
+    Args:
+    page (int): The current page number (1-indexed).
+    page_size (int): The number of items per page.
+
+    Returns:
+    List[List]: The list of rows corresponding to the current page.
+    """
+    assert isinstance(page, int) and page > 0
+    assert isinstance(page_size, int) and page_size > 0
+
+    dataset = self.dataset()
+    start_index, end_index = index_range(page, page_size)
+
+    # Return the appropriate slice of the dataset
+    if start_index >= len(dataset):
+        return []
+    return dataset[start_index:end_index]
