4f15f0f0ad4beb4cfbd0a201526ce15eaed350b3
mahakamal-e <mahakamal09@gmail.com>
2024-07-24 18:06:25 +0000

    task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" FIFOCache module"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class inherits from BaseCaching"""
+
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.queue.remove(key)
+            self.queue.append(key)
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                oldest_key = self.queue.pop(0)
+                del self.cache_data[oldest_key]
+                print("DISCARD:", oldest_key)
+
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """ Get an item by key"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
