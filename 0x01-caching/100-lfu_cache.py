10378c2de36bfd43b82ef245da5174e597d9c96f
mahakamal-e <mahakamal09@gmail.com>
2024-07-24 18:24:36 +0000

    edit

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -44,7 +44,7 @@ class LFUCache(BaseCaching):
                 if not self.order[self.min_freq]:
                     del self.order[self.min_freq]
                     self.min_freq += 1
- 
+
             self.cache_data[key] = item
             self.frequency[key] = 1
             if 1 not in self.order:
