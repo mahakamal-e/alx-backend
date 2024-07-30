7b794d48f412ac68dea62e16c0428df7ec446b61
mahakamal-e <mahakamal09@gmail.com>
2024-07-30 21:39:52 +0000

    1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,7 +8,7 @@ class Config:
     """ Configration class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = ["UTC"]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
