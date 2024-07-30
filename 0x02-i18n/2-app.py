9d3ebd9ea629ffe69bf571a195b4a633b77334bb
mahakamal-e <mahakamal09@gmail.com>
2024-07-30 17:52:03 +0000

    task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Define Flask-Babel Module """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ Configration class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = ["UTC"]
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale():
+    """ Select language based on user prefrences """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index() -> str:
+    """ render index.html """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
