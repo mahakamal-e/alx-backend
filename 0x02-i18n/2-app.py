7cbfc70c4280e6779ed39d374206ab296ce0cdf2
mahakamal-e <mahakamal09@gmail.com>
2024-07-30 21:42:30 +0000

    edit task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Define Flask-Babel Module """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
