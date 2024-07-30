f1d605e8df681f5c0a49cf90631d2ce1760bc03e
mahakamal-e <mahakamal09@gmail.com>
2024-07-30 21:37:03 +0000

    edit 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -10,12 +10,13 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = ["UTC"]
 
+
 app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
     """ render index.html """
     return render_template('1-index.html')
