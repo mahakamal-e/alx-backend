875d6d1cca0b143fd4b6929b47b613b7b505d23c
mahakamal-e <mahakamal09@gmail.com>
2024-07-30 22:19:38 +0000

    edit 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,16 +8,19 @@ class Config:
     """ Configration class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = ["UTC"]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
 
+
 @babel.localeselector
 def get_locale():
     """ Select language based on user prefrences """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index() -> str:
