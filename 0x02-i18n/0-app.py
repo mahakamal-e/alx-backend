9ae20405ef8130bad0009fd89613422df27d186b
mahakamal-e <mahakamal09@gmail.com>
2024-07-30 21:35:35 +0000

    edit

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
     """Route index.html"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
