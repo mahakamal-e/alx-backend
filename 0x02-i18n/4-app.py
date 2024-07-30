432b29885a859b8218e559abef815f3f1326f48c
mahakamal-e <mahakamal09@gmail.com>
2024-07-30 22:00:38 +0000

    fix task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -8,11 +8,13 @@ class Config:
     """ Configration class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = ["UTC"]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
+
 
 @babel.localeselector
 def get_locale():
@@ -22,10 +24,11 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index() -> str:
     """ render index.html """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
